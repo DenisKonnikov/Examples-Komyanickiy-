1a0648d2c199d702a0ac3dc1a29599578f794532
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-17 22:11:37 +1100

    Второй раз закоммитили Program.cs

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.WriteLine("Введите ваше имя:");// Приглашение ко вводу 
+//string username = Console.ReadLine(); // вводим имя usermane
+//Console.WriteLine("Привет, "); // здесь мы просто печатаем что-то, в данном случа Привет (здороваемся с тем, кого вносим выше)
+//Console.WriteLine(username);// выводится имя, которое мы внесли в строке 1
+
+//Убрали LINE - все будет писаться в одной строке
+Console.Write("Введите ваше имя:");
+string username = Console.ReadLine(); 
+Console.Write("Привет, "); 
+Console.Write(username);
+
+
