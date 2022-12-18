63defad06e656af84ce004e573c54ba839313921
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-18 13:58:04 +1100

    Пораьотали с Машей

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");// выводится на терминале 
+string username = Console.ReadLine();// вводится в терминале
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+} 
+else 
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+    
+}
