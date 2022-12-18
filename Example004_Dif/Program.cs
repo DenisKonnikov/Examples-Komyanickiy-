16d638cb72eb8a14ab12e183ab8b977a4c14ab3a
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-18 12:56:11 +1100

    Делили числа

---
+++ Example004_Dif/Program.cs
@@ -0,0 +1,9 @@
+﻿//Целые числа 
+//int numberA = 10; // ввели целое int число
+//int numberB = 5;
+//Console.WriteLine(numberA/numberB); 
+
+//Вещественные цисла
+double numberA = 12; // ввели целое int число
+double numberB = 5;
+Console.WriteLine(numberA/numberB); 
