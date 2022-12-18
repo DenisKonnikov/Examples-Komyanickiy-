33f9fb770b22f83aa0fd8265df9d1f54c3390d3c
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-18 12:14:28 +1100

    Ссумировали два числа

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿//Вариант1 
+
+//int numberA = 3; // ввели целое int число
+//int numberB = 5;
+//Console.WriteLine(numberA+numberB); 
+
+int numberA = 3; // ввели целое int число
+int numberB = 5;
+int result = numberA+numberB; // ввели новыю переменную для суммы чисел
+Console.WriteLine(result); 
