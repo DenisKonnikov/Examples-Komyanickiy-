20083819823dcf8f354434a3f56882527274846d
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-18 13:18:28 +1100

    Суммировал случайные цыфры и закоммител с основной папки

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -4,7 +4,15 @@
 //int numberB = 5;
 //Console.WriteLine(numberA+numberB); 
 
-int numberA = 3; // ввели целое int число
-int numberB = 5;
+// int numberA = 3; // ввели целое int число
+// int numberB = 5;
+// int result = numberA+numberB; // ввели новыю переменную для суммы чисел
+// Console.WriteLine(result); 
+
+// счет случайных чисел
+int numberA = new Random(). Next(1,10); // любое число от 1 до 9
+Console.WriteLine(numberA); // показывает какое число выбралось
+int numberB = new Random(). Next(1,10);
+Console.WriteLine(numberB); 
 int result = numberA+numberB; // ввели новыю переменную для суммы чисел
 Console.WriteLine(result); 
