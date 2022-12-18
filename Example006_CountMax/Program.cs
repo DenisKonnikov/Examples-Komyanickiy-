6bb0477f19232d43e327ba5a4b01ce6a60179622
Konnikov_Denis <dkonnikov.dk@mail.ru>
2022-12-18 14:57:41 +1100

    Нашли максимальную гирю

---
+++ Example006_CountMax/Program.cs
@@ -0,0 +1,33 @@
+﻿//Вариант 1
+
+// int a = 1;
+// int b = 2;
+// int c = 6;
+// int d = 8;
+// int e = 4;
+// int max = a; // определяем первую гирю как максимальную
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+
+// Console.Write ("max= ");
+// Console.WriteLine (max);
+
+//Вариант 2
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a; // определяем первую гирю как максимальную
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max= ");
+Console.WriteLine (max);
